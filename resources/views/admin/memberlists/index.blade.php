bcd420f06790c026f2da0e547c997277dd25dfc6
root <kristiantokarim@gmail.com>
2017-11-16 22:01:56 +0700

    member list index

---
+++ resources/views/admin/memberlists/index.blade.php
@@ -0,0 +1,52 @@
+@extends('admin.app')
+
+@section('title')
+    Member List
+@endsection
+@section('page_title')
+    <span class="text-semibold">Member List</span> - Show All
+    <button type="button" class="btn btn-success" onclick="window.location.href='{{ route('memberlists.create') }}'">Create</button>
+@endsection
+@section('content')
+    <div class="panel panel-flat">     
+
+        <table class="table datatable-pagination">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>E-mail</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($datas as $data)
+                    <tr>
+                        <td>
+                            {{ $data->name }}
+                        </td>
+                        <td>
+                            {{ $data->email }}
+                        </td>
+                        <td>
+                        <ul class="icons-list">
+                        <li>
+                        {{ Form::open(array('method' => 'GET', 'url' => route('memberlists.edit', $data->id))) }}
+                        <button type="submit" class="btn btn-primary"><i class="icon-pencil"></i> Edit</button>
+                        {{ Form::close() }}
+                        </li>
+                        <li>
+                        {{ Form::open(array('method' => 'DELETE', 'url' => route('memberlists.destroy', $data->id))) }}
+                        <button type="submit" class="btn btn-danger"><i class="icon-trash"></i> Delete</button>
+                        {{ Form::close() }}
+                        </li>
+                        </ul>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+{{ $datas->links() }}
+    </div>
+
+@endsection
