e199dd67accd5bddad11abd0b370d0699ba1ed7e
root <kristiantokarim@gmail.com>
2017-11-23 14:44:54 +0700

    fix minor changerequest

--- resources/views/admin/navbar.blade.php
+++ resources/views/admin/navbar.blade.php
@@ -125,7 +125,7 @@
                     <span>{{ Auth::user()->name }}</span>
                     <i class="caret"></i>
                 </a>
-                <ul class="dropdown-menu dropdown-menu-right">
+                <!--ul class="dropdown-menu dropdown-menu-right">
                     <li><a href="#"><i class="icon-user-plus"></i> My profile</a></li>
                     <li><a href="#"><i class="icon-coins"></i> My balance</a></li>
                     <li><a href="#"><span class="badge bg-teal-400 pull-right">58</span> <i class="icon-comment-discussion"></i> Messages</a></li>
@@ -135,7 +135,7 @@
                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                         {{ csrf_field() }}
                     </form>
-                </ul>
+                </ul-->
             </li>
         </ul>
     </div>
