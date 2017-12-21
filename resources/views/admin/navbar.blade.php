eceb57c35ba780cb0df9817ab8f5240f9bc169df
Kristianto Karim <kristiantokarim@gmail.com>
2017-12-21 17:02:19 +0700

    show logout

--- resources/views/admin/navbar.blade.php
+++ resources/views/admin/navbar.blade.php
@@ -125,17 +125,17 @@
                     <span>{{ Auth::user()->name }}</span>
                     <i class="caret"></i>
                 </a>
-                <!--ul class="dropdown-menu dropdown-menu-right">
-                    <li><a href="#"><i class="icon-user-plus"></i> My profile</a></li>
+                <ul class="dropdown-menu dropdown-menu-right">
+                    <!--li><a href="#"><i class="icon-user-plus"></i> My profile</a></li>
                     <li><a href="#"><i class="icon-coins"></i> My balance</a></li>
                     <li><a href="#"><span class="badge bg-teal-400 pull-right">58</span> <i class="icon-comment-discussion"></i> Messages</a></li>
                     <li class="divider"></li>
-                    <li><a href="#"><i class="icon-cog5"></i> Account settings</a></li>
+                    <li><a href="#"><i class="icon-cog5"></i> Account settings</a></li-->
                     <li><a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><i class="icon-switch2"></i> Logout</a></li>
                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                         {{ csrf_field() }}
                     </form>
-                </ul-->
+                </ul>
             </li>
         </ul>
     </div>
