79feaab1ca57add9e70e85683a22e635745b8676
Rio Chandra Rajagukguk <riochr17@gmail.com>
2018-01-09 23:28:48 +0700

    perbaikan webview (2)

--- resources/views/payment/pay.blade.php
+++ resources/views/payment/pay.blade.php
@@ -11,7 +11,7 @@
                 key: "d1df1e4dc0075d52b721a9c2a67598ee",
                 paymentId: "<?php echo $payData['payment_id'] ?>",
                 backUrl: "<?php echo $payData['callback_url'] ?>"
-            },
+            }
 
             window.location.href = SGOSignature.getIframeURL(data);
 
