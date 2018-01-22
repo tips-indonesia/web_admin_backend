d9daa286b19e65aa5cc0403cc065b18940fa22c9
Kristianto <kristiantokarim@gmail.com>
2018-01-22 20:56:14 +0700

    fix button

--- resources/views/admin/deliveries/show.blade.php
+++ resources/views/admin/deliveries/show.blade.php
@@ -46,7 +46,7 @@
                             </select>
                         </div>
                         <div class="text-right form-group">
-                            <button type="submit" value="save" class="btn btn-primary" name="submit">Save <i class="icon-arrow-right14 position-right" disabled></i></button>
+                            <button type="submit" value="save" class="btn btn-primary" name="submit" disabled>Save <i class="icon-arrow-right14 position-right" ></i></button>
                             <button type="submit" value="post"  class="btn btn-success" name="submit" disabled>Submit <i class="icon-arrow-right14 position-right"></i></button>
                         </div>
             {{ Form::close() }}
