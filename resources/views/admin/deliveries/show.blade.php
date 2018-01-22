c0e28a6ba09ac8eafcf00d1367e87a456250bba2
Kristianto <kristiantokarim@gmail.com>
2018-01-22 20:55:29 +0700

    fix dropoff

--- resources/views/admin/deliveries/show.blade.php
+++ resources/views/admin/deliveries/show.blade.php
@@ -41,7 +41,7 @@
                         <div class="text-right form-group">
                             <select multiple="multiple" class="form-control listbox" name="shipments[]" disabled>
                                @foreach ($shipment_lists as $datax)
-                                    <option value="{{ $datax->id }}" @if (in_array($datax->id, $delivery_shipments)) selected @endif> {{ $datax->shipment_id }} &nbsp; - &nbsp; {{ $datax->transaction_date }} &nbsp; - &nbsp; {{ $datax->origin_name }} &nbsp; - &nbsp; {{ $datax->destination_name }} </option>
+                                    <option value="{{ $datax->id }}" @if (in_array($datax->id, $delivery_shipments)) selected @endif disabled> {{ $datax->shipment_id }} &nbsp; - &nbsp; {{ $datax->transaction_date }} &nbsp; - &nbsp; {{ $datax->origin_name }} &nbsp; - &nbsp; {{ $datax->destination_name }} </option>
                                 @endforeach
                             </select>
                         </div>
