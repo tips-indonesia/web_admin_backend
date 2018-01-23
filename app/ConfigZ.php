af60e502622976592dfc77159f7b7247ced0280f
Rio Chandra Rajagukguk <riochr17@gmail.com>
2018-01-23 23:44:36 +0700

    tambah model konfigurasi

---
+++ app/ConfigZ.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ConfigZ extends Model
+{
+    protected $guarded = array();
+}
---
+++ app/Http/Controllers/ConfigHunter.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\ConfigZ;
+
+class ConfigHunter extends Controller
+{
+	public static $CRON_MINUTES_ROUTINE = "CRON_MINUTES_ROUTINE";
+	public static $CRON_ITERATOR_ROUTINE = "CRON_ITERATOR_ROUTINE";
+	public function test(){
+		ConfigHunter::set("a", "2");
+		ConfigHunter::set("a", "5");
+
+		return ConfigHunter::isExist("a")->value;
+	}
+    public static function isExist($key){
+    	$value = ConfigZ::where('key', $key)->first();
+    	if($value){
+    		return $value;
+    	}else{
+    		return false;
+    	}
+    }
+    public static function set($key, $value){
+    	if($data = ConfigHunter::isExist($key)){
+    		$data->value = $value;
+    		$data->save();
+    		return $data;
+    	}else{
+    		return ConfigZ::create(array(
+    			"key" => $key,
+    			"value" => $value
+    		));
+    	}
+    }
+}
