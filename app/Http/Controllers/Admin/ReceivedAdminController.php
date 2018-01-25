b6dd434e4ab3cd4ea112ffe669347318d63daefd
Kristianto <kristiantokarim@gmail.com>
2018-01-25 17:08:46 +0700

    fix receiv

--- app/Http/Controllers/Admin/ReceivedAdminController.php
+++ app/Http/Controllers/Admin/ReceivedAdminController.php
@@ -49,9 +49,9 @@ class ReceivedAdminController extends Controller
             $shipment_data = Shipment::where('id','!=', 0)->whereIn('id', $shipments);
         }
         if ($flag == true) {
-            $shipment_data = $shipment_data->where('shipment_id', $data['value'])->union(Shipment::where('is_posted', 1)->where('is_take', 1)->get())->paginate(10);
+            $shipment_data = $shipment_data->union(Shipment::where('is_posted',1)->where('is_take',1)->get())->where('shipment_id', $data['value'])->paginate(10);
         } else {
-            $shipment_data = $shipment_data->whereIn('id_shipment_status', [3,4])->union(Shipment::where('is_posted', 1)->where('is_take', 1)->get())->paginate(10);
+            $shipment_data = $shipment_data->union(Shipment::where('is_posted',1)->where('is_take',1)->get())->whereIn('id_shipment_status', [3,4])->paginate(10);
         }
         
         foreach($shipment_data as $ship) {
