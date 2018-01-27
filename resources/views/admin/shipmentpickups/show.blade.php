7eaa3681963b39ab7336e559fd14f875b1bc487c
Andhika Estrada <andhika.dec@gmail.com>
2018-01-27 14:25:25 +0700

    B8RcyX5n : bug fix data take by consignee

--- resources/views/admin/shipmentpickups/show.blade.php
+++ resources/views/admin/shipmentpickups/show.blade.php
@@ -80,11 +80,11 @@
                         <div class="form-group">
                             <label class="display-block text-semibold">Dispatch Type :</label>
                             <label class="radio-inline">
-                                <input type="radio" name="dispatch_type" @if($data->dispatch_type == 'Dispatch to consignee') checked="checked" @endif value="Dispatch to consignee"  disabled readonly>
+                                <input type="radio" name="dispatch_type" @if(!$data->is_take) checked="checked" @endif value="Dispatch to consignee"  disabled readonly>
                                 Dispatch to consignee
                             </label>
                             <label class="radio-inline">
-                                <input type="radio" name="dispatch_type" @if($data->dispatch_type == 'Pickup to consignee') checked="checked" @endif value="Pickup to consignee"  disabled readonly>
+                                <input type="radio" name="dispatch_type" @if($data->is_take) checked="checked" @endif value="Taken by consignee"  disabled readonly>
                                 Taken by consignee
                             </label>
                         </div>
