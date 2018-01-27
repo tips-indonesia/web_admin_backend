f835cabe6af1afc1869da007f748ee6e246d765d
Andhika Estrada <andhika.dec@gmail.com>
2018-01-27 11:14:32 +0700

    GSayGcSo : bug create user

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'username', 'password'
+        'first_name', 'last_name', 'name', 'username', 'password'
     ];
 
     /**
@@ -27,4 +27,11 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    // public static function create($data)
+    // {
+    //     // var_dump("CRE");
+    //     parent::create($data);
+    //     // return dd($data);
+    // }
 }
