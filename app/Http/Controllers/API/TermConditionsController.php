7077725830c51ce9678d317314c39c26cb4e5b34
unknown <leo112071@gmail.com>
2018-02-18 20:47:33 +0700

    API untuk terms

--- app/Http/Controllers/API/TermConditionsController.php
+++ app/Http/Controllers/API/TermConditionsController.php
@@ -5,18 +5,21 @@ namespace App\Http\Controllers\API;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
-use App\Term;
+use App\ConfigZ;
 
 class TermConditionsController extends Controller
 {
     //
     function index(){
-        $terms = Term::all();
+        $terms = ConfigZ::all();
         $data = array(
             'err' => null,
             'result' => $terms
         );
-
+        foreach ($terms as $term) {
+        	$string = explode("<br>", $term->value);
+        	$term->value = $string;
+        }
         return response()->json($data, 200);
     }
 }
