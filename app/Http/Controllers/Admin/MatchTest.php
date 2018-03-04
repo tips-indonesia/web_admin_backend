2e5b5778fe184d8ad05178c25fa6580b9b63ee92
Rio Chandra Rajagukguk <riochr17@gmail.com>
2018-03-04 13:45:57 +0700

    perbaikan match manual

--- app/Http/Controllers/Admin/MatchTest.php
+++ app/Http/Controllers/Admin/MatchTest.php
@@ -9,11 +9,14 @@ use Validator;
 use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Session;
+use App\Http\Controllers\UtilityController;
 
 class MatchTest extends Controller
 {
     public function index()
     {
+    	$u = new UtilityController;
+    	$u->CekDataAntrian();
         return view('admin.match.index');
     }
 }
