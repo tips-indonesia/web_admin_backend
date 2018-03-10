d05837f8f7e715a16abbb730d8b414e7d62802ca
Rio Chandra Rajagukguk <riochr17@gmail.com>
2018-03-10 16:49:43 +0700

    perbaikan seeder baru 4

--- database/seeds/EtcSeeder.php
+++ database/seeds/EtcSeeder.php
@@ -18,9 +18,22 @@ class EtcSeeder extends Seeder
         // app()['cache']->forget('spatie.permission.cache');
 
         // $p1 = Permission::create(['name' => 'deliveryshipment.', 'show_name' => 'Delivery Shipment']);
-        $p2 = Permission::create(['name' => 'receivedarrivalprocessingcenter.', 'show_name' => 'Received by Arrival Processing Center']);
+        // $p2 = Permission::create(['name' => 'receivedarrivalprocessingcenter.', 'show_name' => 'Received by Arrival Processing Center']);
         
     	// $role->givePermissionTo($p1);
-    	$role->givePermissionTo($p2);
+    	// $role->givePermissionTo($p2);
+
+
+        MenuList::create([
+            'name' => 'Received by Arrival Processing Center',
+            'menu_parent_id' => $transaction->id,
+            'class_name' => 'receivedarrivalprocessingcenter.'
+        ]);
+        
+            MenuList::create([
+                'name' => 'Delivery Shipment',
+                'menu_parent_id' => $transaction->id,
+                'class_name' => 'deliveryshipment.'
+            ]);
     }
 }
