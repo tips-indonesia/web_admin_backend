da387cc15aa702c8866c3d3ae5712c49e1910497
unknown <leo112071@gmail.com>
2018-03-10 16:33:43 +0700

    Delivery Shipment done

--- app/Http/Controllers/Admin/DeliveryShipmentAdminController.php
+++ app/Http/Controllers/Admin/DeliveryShipmentAdminController.php
@@ -4,10 +4,92 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\PackagingList;
+use App\SlotList;
+use App\Shipment;
+use App\ShipmentStatus;
+use App\CityList;
+use App\PaymentType;
+use App\BankList;
+use Illuminate\Support\Facades\Input;
 
 class DeliveryShipmentAdminController extends Controller
 {
     public function index() {
-    	return view('admin.deliveryshipment.index');
+    	$package = null;
+    	$flag = false;
+        if (Input::get('param') == 'blank' || !Input::get('param')) {
+            $data['param'] = Input::get('param');
+            $data['value'] = Input::get('value');
+        } else {
+            $data['param'] = Input::get('param');
+            $data['value'] = Input::get('value');
+            $flag = true;
+        }
+
+        if ($flag == true) {
+        	$package = PackagingList::join('slot_lists', 'packaging_lists.id_slot', '=', 'slot_lists.id')
+    							->join('shipments', 'shipments.id_slot', '=', 'slot_lists.id')
+    						    ->select('packaging_lists.*', 
+                                         'slot_lists.id_slot_status', 
+                                         'slot_lists.slot_id', 
+                                         'shipments.id_shipment_status', 
+                                         'slot_lists.origin_city',
+                                         'slot_lists.destination_city')
+    						    ->where('slot_lists.id_slot_status', '7')
+    						    ->where(Input::get('param'), Input::get('value'))
+    						    ->distinct()
+    						    ->get();        			
+        } else {
+        	$package = PackagingList::join('slot_lists', 'packaging_lists.id_slot', '=', 'slot_lists.id')
+    							->join('shipments', 'shipments.id_slot', '=', 'slot_lists.id')
+    						    ->select('packaging_lists.*', 
+                                             'slot_lists.id_slot_status', 
+                                             'slot_lists.slot_id', 
+                                             'shipments.id_shipment_status', 
+                                             'slot_lists.origin_city',
+                                             'slot_lists.destination_city')
+    						    ->where('slot_lists.id_slot_status', '7')
+    						    ->distinct()
+    						    ->paginate(10);
+        }
+
+    	$data['packages'] = $package;
+
+    	return view('admin.deliveryshipment.index', $data);
+    }
+
+    public function show($id) {
+    	$package = PackagingList::find($id);
+    	$shipments = Shipment::where('id_slot', $package->id_slot)->get();
+
+    	foreach ($shipments as $shipment) {
+    		if ($shipment->id_origin_city == null) $shipment['origin_city'] = null;
+    		else $shipment['origin_city'] = CityList::find($shipment->id_origin_city)->name;
+
+    		if ($shipment->id_destination_city == null) $shipment['destination_city'] = null;
+    		else $shipment['destination_city'] = CityList::find($shipment->id_destination_city)->name;
+
+    		if ($shipment->id_shipment_status == null) $shipment['shipment_status'] = null;
+    		else $shipment['shipment_status'] = ShipmentStatus::find($shipment->id_shipment_status)->description;
+
+    		if ($shipment->id_payment_type == null) $shipment['payment_type'] = null;
+    		else $shipment['payment_type'] = PaymentType::find($shipment->id_payment_type)->name;
+    		
+    		if ($shipment->id_bank == null) $shipment['bank_name'] = null;
+    		else $shipment['bank_name'] = BankList::find($shipment->id_bank)->name;
+    	}
+    	$data['shipments'] = $shipments;
+    	return view('admin.deliveryshipment.show', $data);
+    }
+
+    public function update($id) {
+    	$package = PackagingList::find($id);
+
+    	$package->is_open = 1;
+
+    	$package->save();
+
+    	return back();
     }
 }
---
+++ resources/views/admin/deliveryshipment/show.blade.php
@@ -0,0 +1,234 @@
+@extends('admin.app')
+
+@section('title')
+    Delivery Shipment
+@endsection
+@section('page_title')
+    <span class="text-semibold">Delivery Shipment</span> - Detail
+@endsection
+	
+@section('content')
+	@foreach($shipments as $shipment)
+		<div class="row">
+        <div class="col-md-12">
+            <div class="panel panel-flat">
+                <div class="panel-body">
+                	<h5>Shipment ID : {{$shipment->shipment_id}}</h5>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label>Origin City :</label>
+                                <input class="form-control" type="text" value="{{$shipment->origin_city}}" disabled>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label>Destination City :</label>
+                                <input class="form-control" type="text" value="{{$shipment->destination_city}}" disabled>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="display-block text-semibold">Class Type :</label>
+                            <input  type="radio" name="class_type" @if($shipment->is_first_class == 1) checked="checked" @endif value="0"  readonly disabled>
+                            Regular
+                        </label>
+
+                        <label class="radio-inline">
+                            <input  type="radio" name="class_type" @if($shipment->is_first_class == 0) checked="checked" @endif value="1"  readonly disabled>
+                            First Class
+                        </label>
+                    </div>
+                    <div class="form-group">
+                        <label class="display-block text-semibold">Dispatch Type :</label>
+                            <label class="radio-inline">
+                                <input type="radio" name="dispatch_type" @if($shipment->dispatch_type == 'Dispatch to consignee') checked="checked" @endif value="Dispatch to consignee" readonly disabled>
+                                Dispatch to consignee
+                            </label>
+                            <label class="radio-inline">
+                                <input type="radio" name="dispatch_type" @if($shipment->dispatch_type == 'Pickup to consignee') checked="checked" @endif value="Pickup to consignee" readonly disabled>
+                                Taken by consignee
+                            </label>
+                    </div>
+                    <div class="form-group">
+                        <label>Shipment Status :</label>
+                        <input class="form-control" type="text" name="shipment-status" value="{{$shipment->shipment_status}}" disabled>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label>Received By :</label>
+                                <input class="form-control" type="text" name="receivedby" value="{{$shipment->received_by}}" disabled>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label>Received Time :</label>
+                                <div class="input-group">
+                                    <span class="input-group-addon"><i class="icon-calendar5"></i></span>
+                                    <input type="text" name="received_date" class="form-control pickadate-year" value="{{$shipment->received_time}}">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    @if ($shipment->received_image != null)
+                    <div class="form-group">
+                    	<label>Received Image:</label><br>
+                    	<img src="{{asset($shipment->received_image)}}" width="250">
+                    </div>
+                    @endif
+            <div class="tabbable">
+                        <ul class="nav nav-tabs nav-tabs-highlight nav-justified">
+                            <li class="active"><a href="#shipper_consignee" data-toggle="tab">Shipper & Consignee</a></li>
+                            <li><a href="#goods_cost" data-toggle="tab">Goods Detail & Cost</a></li>
+                            <li><a href="#payment" data-toggle="tab">Payment</a></li>
+                            <li><a href="#notes" data-toggle="tab">Additional Notes</a></li>
+                        </ul>
+
+                        <div class="tab-content">
+                            <div class="tab-pane active" id="shipper_consignee">
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <legend class="text-bold">Shipper</legend>
+                                        <div class="form-group">
+                                            <label>Name :</label>
+                                            <input type="text" class="form-control" value="{{$shipment->shipper_first_name.' '.$shipment->shipper_last_name}}" disabled>
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Address :</label>
+                                            <textarea rows="5" class="form-control" placeholder="Enter shipper address here" name="shipper_address" readonly disabled>{{ $shipment->shipper_address }}</textarea>
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Mobile Phone :</label>
+                                            {{ Form::text('shipper_mobile', $shipment->shipper_mobile_phone, array('class' => 'form-control', 'placeholder' => 'Shipper Mobile Phone', 'readonly' => 'readonly', 'disabled' => 'disabled')) }}
+                                        </div>
+                                        <div class="form-group">
+                                            <label>E-mail :</label>
+                                            {{ Form::email('shipper_email_address', $shipment->shipper_email_address, array('class' => 'form-control', 'placeholder' => 'Shipper E-mail address', 'readonly' => 'readonly', 'disabled' => 'disabled')) }}
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Latitude :</label>
+                                            {{ Form::text('shipper_latitude', $shipment->shipper_latitude, array('class' => 'form-control', 'placeholder' => 'Shipper Latitude', 'readonly' => 'readonly', 'disabled' => 'disabled')) }}
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Longitude :</label>
+                                            {{ Form::text('shipper_longitude', $shipment->shipper_longitude, array('class' => 'form-control', 'placeholder' => 'Shipper Longitude', 'readonly' => 'readonly', 'disabled' => 'disabled')) }}
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <legend class="text-bold">Consignee</legend>
+                                        <div class="form-group">
+                                            <label>Name :</label>
+                                            {{ Form::text('consignee_name', $shipment->consignee_first_name.' '.$shipment->consignee_last_name, array('class' => 'form-control', 'readonly' => 'readonly', 'disabled' => 'disabled')) }}
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Address :</label>
+                                            <textarea rows="5" class="form-control" placeholder="Enter consignee address here" name="consignee_address" readonly disabled>{{ $shipment->consignee_address }}</textarea>
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Phone Number :</label>
+                                            {{ Form::text('consignee_phone', $shipment->consignee_phone_no, array('class' => 'form-control', 'placeholder' => 'Consignee Phone Number', 'readonly' => 'readonly', 'disabled' => 'disabled')) }}
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Mobile Phone :</label>
+                                            {{ Form::text('consignee_mobile', $shipment->consignee_mobile_phone, array('class' => 'form-control', 'placeholder' => 'Consignee Mobile Phone', 'readonly' => 'readonly', 'disabled' => 'disabled')) }}
+                                        </div>
+                                        <div class="form-group">
+                                            <label>E-mail :</label>
+                                            {{ Form::email('consignee_email_address', $shipment->consignee_email_address, array('class' => 'form-control', 'placeholder' => 'Consignee E-mail address', 'readonly' => 'readonly', 'disabled' => 'disabled')) }}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="tab-pane" id="goods_cost">
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <legend class="text-bold">Goods Detail</legend>
+                                        <div class="form-group">
+                                            <label>Shipment Content :</label>
+                                            {{ Form::text('shipment_content', $shipment->shipment_contents, array('class' => 'form-control', 'placeholder' => 'Shipment Content', 'readonly' => 'readonly', 'disabled' => 'disabled')) }}
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Estimated Goods Value :</label>
+                                            {{ Form::text('estimated_goods_value', $shipment->estimate_goods_value, array('class' => 'form-control', 'placeholder' => 'Estimated Goods Value' ,'readonly' => 'readonly', 'disabled' => 'disabled')) }}
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Estimated Weight :</label>
+                                            {{ Form::number('estimated_weight', $shipment->estimate_weight, array('class' => 'form-control', 'placeholder' => 'Estimated Weight', 'readonly' => 'readonly', 'disabled' => 'disabled')) }}
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <legend class="text-bold">Costs</legend>
+                                        <div class="form-group">
+                                            <label class="display-block text-semibold">Additional Insurance :</label>
+                                            <label class="radio-inline">
+                                                <input type="radio" name="additional_insurance" @if($shipment->is_add_insurance == 0) checked="checked" @endif value="0" disabled readonly>
+                                                No
+                                            </label>
+
+                                            <label class="radio-inline">
+                                                <input type="radio" name="additional_insurance" @if($shipment->is_add_insurance == 1) checked="checked" @endif  value="1" disabled readonly>
+                                                Yes
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="tab-pane" id="payment">
+                                <div class="form-group">
+                                    <label class="display-block text-semibold">Online Payment :</label>
+                                    <label class="radio-inline">
+                                        <input type="radio" name="online_payment" @if($shipment->is_online == 0) checked="checked" @endif  value="0" disabled readonly>
+                                        No
+                                    </label>
+
+                                    <label class="radio-inline">
+                                        <input type="radio" name="online_payment" @if($shipment->is_online == 1) checked="checked" @endif  value="1" disabled readonly>
+                                        Yes
+                                    </label>
+                                </div>
+                                <div class="form-group">
+                                    <label>Payment Type :</label>
+                                    <input type="text" value="{{$shipment->payment_type}}" class="form-control" disabled>
+                                </div>
+                                <div class="form-group">
+                                    <label>Bank Name :</label>
+                                    <input type="text" value="{{$shipment->bank_name}}" class="form-control" disabled="">
+                                </div>
+                                <div class="form-group">
+                                    <label>Card Type :</label>
+                                    <select name="card_type" class="select-search" id="card" disabled readonly>
+                                    </select>
+                                </div>
+                                <div class="form-group">
+                                    <label>Card Number :</label>
+                                    {{ Form::number('card_number', $shipment->card_no, array('class' => 'form-control', 'placeholder' => 'Card Number', 'disabled'=> 'disabled')) }}
+                                </div>
+                                <div class="form-group">
+                                    <label>Security Code :</label>
+                                    {{ Form::number('security_code', $shipment->card_security_code, array('class' => 'form-control', 'placeholder' => 'Card Security Number', 'disabled'=> 'disabled', 'readonly' => 'readonly')) }}
+                                </div>
+                                <div class="form-group">
+                                    <label>Expired Date :</label>
+                                    <div class="input-group">
+                                        <span class="input-group-addon"><i class="icon-calendar5"></i></span>
+                                        <input type="text" name="expired_date" class="form-control pickadate-year" placeholder="Expired date" value="{{ $shipment->card_expired_date }}" disabled>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="tab-pane" id="notes">
+                                <div class="form-group">
+                                    <label>Additional Notes :</label>
+                                    <textarea rows="10" class="form-control" placeholder="Additional Notes" name="addtional_notes" disabled readonly>{{ $shipment->add_notes }}</textarea>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div> 
+        </div>
+	@endforeach
+@endsection
