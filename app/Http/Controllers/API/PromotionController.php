c91c630d68e2fa34319739ca077d9e7f89da634e
unknown <leo112071@gmail.com>
2018-03-17 17:35:31 +0700

    API PROMO

--- app/Http/Controllers/API/PromotionController.php
+++ app/Http/Controllers/API/PromotionController.php
@@ -11,33 +11,31 @@ use Illuminate\Support\Facades\URL;
 class PromotionController extends Controller
 {
     public function getPromo() {
-    	// if (isset($_GET['id_user'])) {
-    	// 	// Dunno what to do
-    	// } else {
-    	// 	// Dunno what to do
-    	// }
+    	if (isset($_GET['id_user'])) {
+    		// Dunno what to do
+    	} else {
+    		// Dunno what to do
+    	}
 
-    	// $promos = Promotion::all();
-    	// $res = array();
+    	$promos = Promotion::all();
+    	$res = array();
 
-    	// foreach($promos as $promo) {
-    	// 	$dum = [
-    	// 		'id' => $promo->id,
-    	// 		'img_src' => $promo->filename,
-    	// 		'title' => $promo->title,
-    	// 		'description' => $promo->description
-    	// 	];
-    	// 	array_push($res, $dum);
-    	// }
+    	foreach($promos as $promo) {
+    		$dum = [
+    			'id' => $promo->id,
+    			'img_src' => URL::to('storage/promotions/'.$promo->filename),
+    			'title' => $promo->title,
+    			'description' => $promo->description
+    		];
+    		array_push($res, $dum);
+    	}
 
-    	// $data = array(
-    	// 	'err' => null,
-    	// 	'result' => $res
-    	// );
+    	$data = array(
+    		'err' => null,
+    		'result' => $res
+    	);
 
-    	// return response()->json($data, 200);
-
-    	return URL::to(storage_path());
+    	return response()->json($data, 200);
     }
 
     public function getIklan() {
@@ -53,7 +51,7 @@ class PromotionController extends Controller
     	foreach($iklans as $iklan) {
     		$dum = [
     			'id' => $iklan->id,
-    			'img_src' => $iklan->filename,
+    			'img_src' => URL::to('storage/banners/'.$iklan->filename),
     			'title' => $iklan->title,
     			'description' => $iklan->description
     		];
