9324586441b273910f96add00f24370d7a36076b
Rio Chandra Rajagukguk <riochr17@gmail.com>
2018-03-17 17:44:07 +0700

    create permission seeder

--- database/seeds/EtcSeeder.php
+++ database/seeds/EtcSeeder.php
@@ -21,11 +21,25 @@ class EtcSeeder extends Seeder
 
         // $p1 = Permission::create(['name' => 'deliveryshipment.', 'show_name' => 'Delivery Shipment']);
         // $p2 = Permission::create(['name' => 'receivedarrivalprocessingcenter.', 'show_name' => 'Received by Arrival Processing Center']);
-        $p3 = Permission::create(['name' => 'tipsterpayments.', 'show_name' => 'Tipster Payment']);
+
+
+        MenuList::create([
+            'name' => 'Promotions',
+            'menu_parent_id' => 37,
+            'class_name' => 'promotions.'
+        ]);
+        MenuList::create([
+            'name' => 'Banners',
+            'menu_parent_id' => 37,
+            'class_name' => 'banners.'
+        ]);
+        $p3 = Permission::create(['name' => 'promotions.', 'show_name' => 'Promotions']);
+        $p4 = Permission::create(['name' => 'banners.', 'show_name' => 'Banners']);
         
     	// $role->givePermissionTo($p1);
     	$role = Role::all()->first();
-    	$role->givePermissionTo($p3);
+        $role->givePermissionTo($p3);
+        $role->givePermissionTo($p4);
 
 
         // MenuList::create([
