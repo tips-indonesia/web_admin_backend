918dce5824db2d25115134cfece9179a67f13ebf
Rio Chandra Rajagukguk <riochr17@gmail.com>
2018-03-22 00:24:32 +0700

    perbaikan upload max size 2

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -51,8 +51,8 @@ class Handler extends ExceptionHandler
         if ($exception instanceof \Symfony\Component\HttpFoundation\File\Exception\FileException) {
             // create a validator and validate to throw a new ValidationException
             return Validator::make($request->all(), [
-                'your_file_input' => 'required|file|size:5000',
-            ])->validate();
+                'file' => 'max:5120', //5MB 
+            ]);
         }
 
         return parent::render($request, $exception);
