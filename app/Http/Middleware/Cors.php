b9aabdaf4c704688ebaee4c8e5880030adea0d9f
Rio Chandra Rajagukguk <riochr17@gmail.com>
2018-03-22 11:47:42 +0700

    perbaikan header intermediate 2

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -18,8 +18,8 @@ class Cors
         return $next($request)
             ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Credentials: true')
+            ->header('Access-Control-Allow-Credentials', 'true')
             ->header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-            ->header('Access-Control-Expose-Headers: Access-Control-Allow-Methods, Access-Control-Allow-Credentials, Access-Control-Allow-Headers');
+            ->header('Access-Control-Expose-Headers', 'Access-Control-Allow-Methods, Access-Control-Allow-Credentials, Access-Control-Allow-Headers');
     }
 }
