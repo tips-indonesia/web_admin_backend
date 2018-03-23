189fe72ad3ccf3decc74fba399f6dbfc1669fa3f
Rio Chandra Rajagukguk <riochr17@gmail.com>
2018-03-23 10:42:09 +0700

    perbaikan vmecontroller 6

--- app/Http/Controllers/API/VMEController.php
+++ app/Http/Controllers/API/VMEController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 
 use App\VMemberEmployee;
 use App\User;
