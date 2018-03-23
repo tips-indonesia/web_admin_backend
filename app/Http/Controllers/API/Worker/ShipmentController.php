aaff63283ec07536d260161d93024d7b363c98d8
Rio Chandra Rajagukguk <riochr17@gmail.com>
2018-03-23 15:25:35 +0700

    perbaikan syntax 3

--- app/Http/Controllers/API/Worker/ShipmentController.php
+++ app/Http/Controllers/API/Worker/ShipmentController.php
@@ -140,8 +140,8 @@ class ShipmentController extends Controller
             $shipment->save();
 
             $ms_user = MemberList::find($shipment->id_shipper);
-            $mess = 'Barang kiriman Anda dengan kode pengiriman ' + $shipment->shipment_id + ' sudah diambil oleh: '
-                    + $shipment->consignee_first_name + " " + $shipment->consignee_last_name;
+            $mess = 'Barang kiriman Anda dengan kode pengiriman ' . $shipment->shipment_id . ' sudah diambil oleh: '
+                    . $shipment->consignee_first_name . " " . $shipment->consignee_last_name;
             if($ms_user)
                 if($ms_user->token) {
                     FCMSender::post(array(
