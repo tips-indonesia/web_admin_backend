e09ee67e4dfb0d011c60c43096a63b9ac98713a0
Rio Chandra Rajagukguk <riochr17@gmail.com>
2018-03-23 10:40:43 +0700

    perbaikan vmecontroller 5

--- app/Http/Controllers/API/VMEController.php
+++ app/Http/Controllers/API/VMEController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 use App\VMemberEmployee;
+use App\User;
 
 class VMEController extends Controller
 {
@@ -30,11 +31,53 @@ class VMEController extends Controller
 	    return response()->json($data, 200);
     }
 
+    public function getUser(Request $request){
+        $user = User::where('username', $request->mobile_phone_no)->first();
+        if($user == null) {
+            $data = array(
+                'err' => [
+                    'code' => 0,
+                    'message' => 'Nomor handphone tidak ditemukan'
+                ],
+                'result' => null
+            );
+        } else {
+            if(!Hash::check($request->password, $user->password)) {
+                $data = array(
+                    'err' => [
+                        'code' => 0,
+                        'message' => 'Password salah'
+                    ],
+                    'result' => null
+                );
+            } else {
+                unset($member_list['password']);
+                $data = array(
+                    'err' => null,
+                    'result' => $member_list
+                );
+            }
+        }
+
+        return response()->json($data, 200);
+    }
+
     public function login(Request $request){
     	$user = VMemberEmployee::where('mobile_phone_no', $request->mobile_phone_no)->first();
-    	if($user->is_employee == 'Y')
-    		return $this->throwException();
-    	else
-    		return (new UserController())->login($request);
+    	if(!$user){
+            $data = array(
+                'err' => [
+                    'code' => 0,
+                    'message' => 'Nomor handphone tidak ditemukan'
+                ],
+                'result' => null
+            );
+            return response()->json($data, 200);
+    	}else{
+	    	if($user->is_employee == 'Y')
+	    		return $this->getUser($request);
+	    	else
+	    		return (new UserController())->login($request);
+	    }
     }
 }
