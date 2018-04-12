ccaa8ed4c0833082724ac88f9a598d36add33a54
Rio Chandra Rajagukguk <riochr17@gmail.com>
2018-04-13 06:16:53 +0700

    log curl

--- app/Http/Controllers/cURLFaker.php
+++ app/Http/Controllers/cURLFaker.php
@@ -16,19 +16,19 @@ class cURLFaker extends Controller
     //
     // --
     public function sendMailTipsterStep1($email, $NAMA, $ANTAR_CODE){
-        exec("sh send_post.sh localhost/api/send_email 'type' 'antar' 'code' '1' 'email' '$email' 'NAMA' '$NAMA' 'ANTAR_CODE' '$ANTAR_CODE'");
+        exec("sh send_post.sh localhost/api/send_email 'type' 'antar' 'code' '1' 'email' '$email' 'NAMA' '$NAMA' 'ANTAR_CODE' '$ANTAR_CODE' >> logcurl.txt");
     }
 
     public function sendMailTipsterStep2($email, $NAMA, $ANTAR_CODE, $JAM_TANGGAL){
-    	exec("sh send_post.sh localhost/api/send_email 'type' 'antar' 'code' '2' 'email' '$email' 'NAMA' '$NAMA' 'ANTAR_CODE' '$ANTAR_CODE' 'JAM_TANGGAL' '$JAM_TANGGAL'");
+    	exec("sh send_post.sh localhost/api/send_email 'type' 'antar' 'code' '2' 'email' '$email' 'NAMA' '$NAMA' 'ANTAR_CODE' '$ANTAR_CODE' 'JAM_TANGGAL' '$JAM_TANGGAL' >> logcurl.txt");
     }
 
     public function sendMailTipsterStep3($email, $NAMA, $ANTAR_CODE, $ORIGIN_AIRPORT_NAME, $_3HOURS_DEPARTURE_TIME, $NOMOR_CALL_CENTER){
-        exec("sh send_post.sh localhost/api/send_email 'type' 'antar' 'code' '3' 'email' '$email' 'NAMA' '$NAMA' 'ANTAR_CODE' '$ANTAR_CODE' 'ORIGIN_AIRPORT_NAME' '$ORIGIN_AIRPORT_NAME' '_3HOURS_DEPARTURE_TIME' '$_3HOURS_DEPARTURE_TIME' 'NOMOR_CALL_CENTER' '$NOMOR_CALL_CENTER'");
+        exec("sh send_post.sh localhost/api/send_email 'type' 'antar' 'code' '3' 'email' '$email' 'NAMA' '$NAMA' 'ANTAR_CODE' '$ANTAR_CODE' 'ORIGIN_AIRPORT_NAME' '$ORIGIN_AIRPORT_NAME' '_3HOURS_DEPARTURE_TIME' '$_3HOURS_DEPARTURE_TIME' 'NOMOR_CALL_CENTER' '$NOMOR_CALL_CENTER' >> logcurl.txt");
     }
 
     public function sendMailTipsterStep7($email, $NAMA){
-        exec("sh send_post.sh localhost/api/send_email 'type' 'antar' 'code' '7' 'email' '$email' 'NAMA' '$NAMA'");
+        exec("sh send_post.sh localhost/api/send_email 'type' 'antar' 'code' '7' 'email' '$email' 'NAMA' '$NAMA' >> logcurl.txt");
     }
     // ------------------------------
     // ##############################
@@ -40,11 +40,11 @@ class cURLFaker extends Controller
     //
     // --
     public function sendMailShipperStep1($email, $NAMA, $SHIPPING_CODE, $NOMOR_CALL_CENTER){
-        exec("sh send_post.sh localhost/api/send_email 'type' 'kirim' 'code' '1' 'email' '$email' 'NAMA' '$NAMA' 'SHIPPING_CODE' '$SHIPPING_CODE' 'NOMOR_CALL_CENTER' '$NOMOR_CALL_CENTER'");
+        exec("sh send_post.sh localhost/api/send_email 'type' 'kirim' 'code' '1' 'email' '$email' 'NAMA' '$NAMA' 'SHIPPING_CODE' '$SHIPPING_CODE' 'NOMOR_CALL_CENTER' '$NOMOR_CALL_CENTER' >> logcurl.txt");
     }
 
     public function sendMailShipperStep8($email, $NAMA, $SHIPPING_CODE, $RECIPIENT_NAME){
-        exec("sh send_post.sh localhost/api/send_email 'type' 'kirim' 'code' '8' 'email' '$email' 'NAMA' '$NAMA' 'SHIPPING_CODE' '$SHIPPING_CODE' 'RECIPIENT_NAME' '$RECIPIENT_NAME'");
+        exec("sh send_post.sh localhost/api/send_email 'type' 'kirim' 'code' '8' 'email' '$email' 'NAMA' '$NAMA' 'SHIPPING_CODE' '$SHIPPING_CODE' 'RECIPIENT_NAME' '$RECIPIENT_NAME' >> logcurl.txt");
     }
     // ------------------------------
     // ##############################
