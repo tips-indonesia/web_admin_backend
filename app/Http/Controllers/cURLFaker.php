e0ae2630e104cb422649ae517df96bf9efcf78a4
Rio Chandra Rajagukguk <riochr17@gmail.com>
2018-05-13 21:01:33 +0700

    fix email sender

--- app/Http/Controllers/cURLFaker.php
+++ app/Http/Controllers/cURLFaker.php
@@ -15,7 +15,7 @@ class cURLFaker extends Controller
     //
     // --
     public function sendMailRegistration($email, $NAMA){
-        dd(exec("sh send_post.sh 'http://127.0.0.1/api/send_email' 'type' 'registration' 'email' '$email' 'NAMA' '$NAMA' >> /var/www/html/tips/zz/logcurlx.txt > /dev/null 2>&1 &"));
+        exec("sh send_post.sh 'http://127.0.0.1/api/send_email' 'type' 'registration' 'email' '$email' 'NAMA' '$NAMA' >> /var/www/html/tips/zz/logcurlx.txt > /dev/null 2>&1 &");
     }
     // ------------------------------
     // ##############################
@@ -27,19 +27,19 @@ class cURLFaker extends Controller
     //
     // --
     public function sendMailTipsterStep1($email, $NAMA, $ANTAR_CODE){
-        exec("sh send_post.sh 'http://127.0.0.1/api/send_email' 'type' 'antar' 'code' '1' 'email' '$email' 'NAMA' '$NAMA' 'ANTAR_CODE' '$ANTAR_CODE' >> logcurl.txt > /dev/null 2>&1 &");
+        exec("sh send_post.sh 'http://127.0.0.1/api/send_email' 'type' 'antar' 'code' '1' 'email' '$email' 'NAMA' '$NAMA' 'ANTAR_CODE' '$ANTAR_CODE' >> /var/www/html/tips/zz/logcurlx.txt > /dev/null 2>&1 &");
     }
 
     public function sendMailTipsterStep2($email, $NAMA, $ANTAR_CODE, $JAM_TANGGAL){
-    	exec("sh send_post.sh 'http://127.0.0.1/api/send_email' 'type' 'antar' 'code' '2' 'email' '$email' 'NAMA' '$NAMA' 'ANTAR_CODE' '$ANTAR_CODE' 'JAM_TANGGAL' '$JAM_TANGGAL' >> logcurl.txt > /dev/null 2>&1 &");
+    	exec("sh send_post.sh 'http://127.0.0.1/api/send_email' 'type' 'antar' 'code' '2' 'email' '$email' 'NAMA' '$NAMA' 'ANTAR_CODE' '$ANTAR_CODE' 'JAM_TANGGAL' '$JAM_TANGGAL' >> /var/www/html/tips/zz/logcurlx.txt > /dev/null 2>&1 &");
     }
 
     public function sendMailTipsterStep3($email, $NAMA, $ANTAR_CODE, $ORIGIN_AIRPORT_NAME, $_3HOURS_DEPARTURE_TIME, $NOMOR_CALL_CENTER){
-        exec("sh send_post.sh 'http://127.0.0.1/api/send_email' 'type' 'antar' 'code' '3' 'email' '$email' 'NAMA' '$NAMA' 'ANTAR_CODE' '$ANTAR_CODE' 'ORIGIN_AIRPORT_NAME' '$ORIGIN_AIRPORT_NAME' '_3HOURS_DEPARTURE_TIME' '$_3HOURS_DEPARTURE_TIME' 'NOMOR_CALL_CENTER' '$NOMOR_CALL_CENTER' >> logcurl.txt > /dev/null 2>&1 &");
+        exec("sh send_post.sh 'http://127.0.0.1/api/send_email' 'type' 'antar' 'code' '3' 'email' '$email' 'NAMA' '$NAMA' 'ANTAR_CODE' '$ANTAR_CODE' 'ORIGIN_AIRPORT_NAME' '$ORIGIN_AIRPORT_NAME' '_3HOURS_DEPARTURE_TIME' '$_3HOURS_DEPARTURE_TIME' 'NOMOR_CALL_CENTER' '$NOMOR_CALL_CENTER' >> /var/www/html/tips/zz/logcurlx.txt > /dev/null 2>&1 &");
     }
 
     public function sendMailTipsterStep7($email, $NAMA){
-        exec("sh send_post.sh 'http://127.0.0.1/api/send_email' 'type' 'antar' 'code' '7' 'email' '$email' 'NAMA' '$NAMA' >> logcurl.txt > /dev/null 2>&1 &");
+        exec("sh send_post.sh 'http://127.0.0.1/api/send_email' 'type' 'antar' 'code' '7' 'email' '$email' 'NAMA' '$NAMA' >> /var/www/html/tips/zz/logcurlx.txt > /dev/null 2>&1 &");
     }
     // ------------------------------
     // ##############################
@@ -51,11 +51,11 @@ class cURLFaker extends Controller
     //
     // --
     public function sendMailShipperStep1($email, $NAMA, $SHIPPING_CODE, $NOMOR_CALL_CENTER){
-        exec("sh send_post.sh 'http://127.0.0.1/api/send_email' 'type' 'kirim' 'code' '1' 'email' '$email' 'NAMA' '$NAMA' 'SHIPPING_CODE' '$SHIPPING_CODE' 'NOMOR_CALL_CENTER' '$NOMOR_CALL_CENTER' >> logcurl.txt > /dev/null 2>&1 &");
+        exec("sh send_post.sh 'http://127.0.0.1/api/send_email' 'type' 'kirim' 'code' '1' 'email' '$email' 'NAMA' '$NAMA' 'SHIPPING_CODE' '$SHIPPING_CODE' 'NOMOR_CALL_CENTER' '$NOMOR_CALL_CENTER' >> /var/www/html/tips/zz/logcurlx.txt > /dev/null 2>&1 &");
     }
 
     public function sendMailShipperStep8($email, $NAMA, $SHIPPING_CODE, $RECIPIENT_NAME){
-        exec("sh send_post.sh 'http://127.0.0.1/api/send_email' 'type' 'kirim' 'code' '8' 'email' '$email' 'NAMA' '$NAMA' 'SHIPPING_CODE' '$SHIPPING_CODE' 'RECIPIENT_NAME' '$RECIPIENT_NAME' >> logcurl.txt > /dev/null 2>&1 &");
+        exec("sh send_post.sh 'http://127.0.0.1/api/send_email' 'type' 'kirim' 'code' '8' 'email' '$email' 'NAMA' '$NAMA' 'SHIPPING_CODE' '$SHIPPING_CODE' 'RECIPIENT_NAME' '$RECIPIENT_NAME' >> /var/www/html/tips/zz/logcurlx.txt > /dev/null 2>&1 &");
     }
     // ------------------------------
     // ##############################
