b9d87f462de1f6cc2adb0b6c496d01e675b4dfa0
Rio Chandra Rajagukguk <riochr17@gmail.com>
2018-05-13 20:31:41 +0700

    tambah semua template email

---
+++ resources/views/mail/register.blade.php
@@ -0,0 +1,7 @@
+<p>
+Hai {{ $NAMA }}, <br/>
+Selamat datang di TIPS, semoga harimu menyenangkan.
+</p>
+<br/>
+<br/>
+<small>Email ini dikirim pada {{ $datetime }}, zona waktu {{ $timezone }}</small><br/> 
