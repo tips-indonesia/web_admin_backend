027af54bdf4cf9f6d7820863e7628c10561b1589
Leo Nadeak <leo112071@gmail.com>
2018-06-23 15:23:34 +0700

    Add promotion text and add worker ability

---
+++ resources/views/admin/addworkerability/edit.blade.php
@@ -0,0 +1,82 @@
+@extends('admin.app')
+
+@section('title')
+    Add Worker Ability to User
+@endsection
+@section('page_title')
+    <span class="text-semibold">Add Worker Ability to User</span> - Edit
+@endsection
+@section('content')
+    <!-- Vertical form options -->
+    <div class="row">
+        <div class="col-md-12">
+            {{ Form::open(array('url' => route('addworkerability.update', $datas->id), 'method' => 'PUT')) }}
+                <div class="panel panel-flat">
+                    <div class="panel-body">
+                        <div class="form-group">
+                            <label>First Name :</label>
+                            {{ Form::text('fname', $datas->first_name, array('class' => 'form-control', 'placeholder' => 'First Name')) }}
+                        </div>
+                        <div class="form-group">
+                            <label>Last Name :</label>
+                            {{ Form::text('lname', $datas->last_name, array('class' => 'form-control', 'placeholder' => 'Last Name')) }}
+                        </div>
+                        <div class="form-group">
+                            <label>Phone Number :</label>
+                            <div class="row">
+                                
+                                    <input type="text" name="mobile_phone_no" class="form-control" value="{{$datas->mobile_phone_no}}" disabled>
+                                
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label>Email :</label>
+                            <input type="email" name="email" class="form-control" value="{{$datas->email}}">
+                        </div>
+                        <input type="hidden" name="worker" value="1">
+                        <div class="form-group">
+                            <label>Birth Date :</label>
+                            <div class="input-group">
+                                <span class="input-group-addon"><i class="icon-calendar5"></i></span>
+                                <input value="{{$datas->birth_date}}" type="text" name="birth_date" id="date" class="form-control pickadate-year">
+                            </div>
+                        </div>
+                        
+                        <div class="form-group">
+                            <label>Role :</label>
+                            <select name="role" class="select-search">
+                                <option selected disabled></option> 
+                                @foreach ($roles as $role)
+                                    <option value="{{ $role->id }}" @if ($datas->hasRole($role)) selected @endif>{{ $role->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label>New Password :</label>
+                            {{ Form::password('password', array('class' => 'form-control', 'placeholder' => 'Password', 'disabled' => 'disabled')) }}
+                        </div>
+                        <div class="form-group">
+                            <label>Office Name :</label>
+                            <select name="office" class="select-search" id="office">
+                                <option disabled selected></option>
+                                @foreach ($offices as $office)
+                                    <option value="{{ $office->id }}"@if($datas->id_office == $office->id) selected @endif>{{ $office->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="text-right form-group">
+                            <button type="submit" class="btn btn-primary">Submit form <i class="icon-arrow-right14 position-right"></i></button>
+                        </div>
+                    </div>
+                </div>
+            {{ Form::close() }}
+        </div>
+    </div>
+        <script>
+            $('.select-search').select2();
+            $('.pickadate-year').datepicker({
+            format: 'yyyy-mm-dd',
+        });
+        </script>
+@endsection
--- resources/views/admin/addworkerability/index.blade.php
+++ resources/views/admin/addworkerability/index.blade.php
@@ -14,12 +14,14 @@
             <div class="panel-body">
             	<label>First Name:</label>
             	<div class="row">
+            		{{ Form::open(array('url' => route('addworkerability.index'), 'method' => 'GET')) }}
             		<div class="col-md-6">
-            			<input type="text" name="first_name" class="form-conthol" placeholder="First Name">
+            			<input type="text" name="firstname" class="form-control" placeholder="First Name" required @if(isset($firstname)) value="{{$firstname}} @endif">
             		</div>
             		<div class="col-md-2">
-            			<button type="submit" class="btn btn-primary"> Search </button>x
+            			<button type="submit" class="btn btn-primary"> Search </button>
             		</div>
+            		{{ Form::close() }}
             	</div>
             	<table class="table datatable-pagination">
             		<thead>
@@ -28,6 +30,30 @@
             			<th>E-mail</th>
             			<th>Actions</th>
             		</thead>
+            		<tbody>
+            			@foreach ($users as $user)
+	                    <tr>
+	                        <td>
+	                            {{ $user->first_name }}
+	                        </td>
+	                        <td>
+	                            {{ $user->last_name }}
+	                        </td>
+	                        <td>
+	                            {{ $user->email }}
+	                        </td>
+	                        <td>
+	                        <ul class="icons-list">
+	                        <li>
+	                        {{ Form::open(array('method' => 'GET', 'url' => route('addworkerability.edit', $user->id))) }}
+	                        <button type="submit" class="btn btn-primary"><i class="icon-pencil"></i> Edit</button>
+	                        {{ Form::close() }}
+	                        </li>
+	                        </ul>
+	                        </td>
+	                    </tr>
+                		@endforeach
+            		</tbody>
             	</table>
             </div>
         </div>
