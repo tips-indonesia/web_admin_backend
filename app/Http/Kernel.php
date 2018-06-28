73285874f7a8606aa6102427eeb1351f0de8aa98
AkhmadBenny <33388179+AkhmadBenny@users.noreply.github.com>
2018-06-28 23:49:10 +0700

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,9 +41,9 @@ class Kernel extends HttpKernel
         'api' => [
             // 'throttle:60,1',
             'bindings',
-            // \App\Http\Middleware\Cors::class,
+            \App\Http\Middleware\Cors::class,
             \App\Http\Middleware\VerifyRequest::class,
-            // \App\Http\Middleware\Cors::class,
+            \App\Http\Middleware\Cors::class,
         ],
     ];
 
