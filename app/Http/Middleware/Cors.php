89f56938c8e45b1d020503f59ba913321b577315
Rio Chandra Rajagukguk <riochr17@gmail.com>
2018-06-28 21:53:41 +0700

    off verify request 2

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,13 +15,6 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
-            ->header('Origin', '*')
-            ->header('Allow', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Credentials', 'true')
-            ->header('Access-Control-Allow-Headers', 'Access-Control-Allow-Origin, Origin, X-Requested-With, Content-Type, app-kind, Accept')
-            ->header('Access-Control-Expose-Headers', 'Access-Control-Allow-Origin, Access-Control-Allow-Methods, Access-Control-Allow-Credentials, Access-Control-Allow-Headers, app-kind');
+        
     }
 }
--- app/Http/Middleware/VerifyRequest.php
+++ app/Http/Middleware/VerifyRequest.php
@@ -28,6 +28,13 @@ class VerifyRequest
             return response()->json($data, 200);
         }
 
-        return $next($request);
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Origin', '*')
+            ->header('Allow', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Credentials', 'true')
+            ->header('Access-Control-Allow-Headers', 'Access-Control-Allow-Origin, Origin, X-Requested-With, Content-Type, app-kind, Accept')
+            ->header('Access-Control-Expose-Headers', 'Access-Control-Allow-Origin, Access-Control-Allow-Methods, Access-Control-Allow-Credentials, Access-Control-Allow-Headers, app-kind');
     }
 }
