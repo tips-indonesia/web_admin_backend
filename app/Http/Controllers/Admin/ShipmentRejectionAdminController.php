218073c1a5d8bc4fba0c63f1c563d8c2dc8f9db1
Leo Nadeak <leo112071@gmail.com>
2018-09-10 16:01:03 +0700

    Revisi

--- app/Http/Controllers/Admin/ShipmentRejectionAdminController.php
+++ app/Http/Controllers/Admin/ShipmentRejectionAdminController.php
@@ -55,7 +55,8 @@ class ShipmentRejectionAdminController extends Controller
             $shipments = $shipments->where('id_origin_city', $office->id_area);
         }
 
-        $shipments = $shipments->whereIn('id_shipment_status', [-1,4])->where('id_slot', null)->paginate(10);
+        $shipments = $shipments->where('id_shipment_status', 4)->orWhere('id_shipment_status', '<', 0)
+                ->where('id_slot', null)->paginate(10);
 
         foreach($shipments as $dat) {
             $dat['name_origin'] = AirportcityList::find($dat->id_origin_city)->name;
