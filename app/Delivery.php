82d7a47acce97c6d8756f3aa405633a4b78bccdf
Rio Chandra Rajagukguk <riochr17@gmail.com>
2018-09-14 11:59:10 +0700

    tambah timer slot 2

--- app/Delivery.php
+++ app/Delivery.php
@@ -3,17 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use App\CronTimer;
 
 class Delivery extends Model
 {
-    public function startCountingLife(){
-    	$slot_id = $this->slot_id;
-        $time_to_execute = 4 * 60 * 60;
-        $ct = CronTimer::first();
-        if($ct){
-            $time_to_execute = $ct->cron_timer;
-        }
-        exec("sh start_timer.sh $time_to_execute $slot_id >> ~/logcurlx.txt > /dev/null 2>&1 &");
-    }
 }
--- app/SlotList.php
+++ app/SlotList.php
@@ -123,4 +123,13 @@ class SlotList extends Model
         SMSSender::T_send_7($NOHP);
     }
 
+    public function startCountingLife(){
+        $slot_id = $this->slot_id;
+        $time_to_execute = 4 * 60 * 60;
+        $ct = CronTimer::first();
+        if($ct){
+            $time_to_execute = $ct->cron_timer;
+        }
+        exec("sh start_timer.sh $time_to_execute $slot_id >> ~/logcurlx.txt > /dev/null 2>&1 &");
+    }
 }
