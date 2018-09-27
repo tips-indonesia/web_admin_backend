d05738f9a0c17119ac9414c718b2c78c8670c988
Rio Chandra Rajagukguk <riochr17@gmail.com>
2018-09-27 22:30:05 +0700

    perbaikan break line

--- app/Http/Controllers/API/TermConditionsController.php
+++ app/Http/Controllers/API/TermConditionsController.php
@@ -15,13 +15,13 @@ class TermConditionsController extends Controller
     	if (isset($_GET['type'])) {
     		$res = ConfigZ::where('key', $_GET['type'])->value('value');
     		$terms = array(
-    			"value" => explode("<br>", $res),
+    			"value" => explode("<br />", $res),
     		);
 
     	} else {
         	$terms = ConfigZ::all();
         	foreach ($terms as $term) {
-	        	$string = explode("<br>", $term->value);
+	        	$string = explode("<br />", $term->value);
 	        	$term->value = $string;
 	        }
         }
