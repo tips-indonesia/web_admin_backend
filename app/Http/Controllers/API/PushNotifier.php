49355550d5a2f48de495755ee8c6e5c367e983d3
Rio Chandra Rajagukguk <riochr17@gmail.com>
2018-10-01 12:00:26 +0700

    perbaikan email reminder

--- app/Http/Controllers/API/PushNotifier.php
+++ app/Http/Controllers/API/PushNotifier.php
@@ -8,9 +8,17 @@ use App\Http\Controllers\FCMSender;
 use App\SlotList;
 use App\MemberList;
 use App\Http\Controllers\SMSSender;
+use App\Http\Controllers\cURLFaker;
 
 class PushNotifier extends Controller
 {
+
+    private $bsc;
+
+    public function __construct(){
+        $this->bsc = new cURLFaker;
+    }
+    
     //
 	public function delivery_push_notifier($message, $user, $slot){
         FCMSender::post([ "type" 	=> 'Delivery', 
@@ -46,6 +54,7 @@ class PushNotifier extends Controller
     	$antar_code = $slot->slot_id;
     	$sms_message = "Sisa waktu Anda tinggal 15 menit lagi untuk konfirmasi kesediaan mengantar barang pada aplikasi TIPS dengan kode pendaftaran penerbangan $antar_code milik Anda";
     	$this->delivery_sms_sender($sms_message, $user);
+        $this->bsc->sendMailTipster15MinBC($user->email, $user->first_name . " " . $user->last_name, $antar_code);
     }
 
     public function _4hours_before_departure_confirmation_timeout($user, $slot){
@@ -55,6 +64,7 @@ class PushNotifier extends Controller
     	$antar_code = $slot->slot_id;
     	$sms_message = "Sisa waktu konfirmasi kesediaan mengantar pada aplikasi TIPS sudah habis, pendaftaran penerbangan $antar_code milik Anda telah dibatalkan.";
     	$this->delivery_sms_sender($sms_message, $user);
+        $this->bsc->sendMailTipsterNoConfirmationCancelled($user->email, $user->first_name . " " . $user->last_name, $antar_code);
     }
 
     public function _15mins_before_pickup_cutoff($user, $slot){
@@ -64,6 +74,7 @@ class PushNotifier extends Controller
     	$antar_code = $slot->slot_id;
     	$sms_message = "Sisa waktu Anda untuk pengambilan barang antaran TIPS dengan kode pendaftaran penerbangan $antar_code pada TIPS Counter tinggal 15 menit. Bila barang antaran tidak diambil sesuai batas waktu tersebut maka kesediaan Anda menjadi TIPSTER otomatis dianggap batal.";
     	$this->delivery_sms_sender($sms_message, $user);
+        $this->bsc->sendMailTipster15MinBP($user->email, $user->first_name . " " . $user->last_name, $antar_code);
     }
 
     public function _2hours_before_departure_pickup_timeout($user, $slot){
@@ -73,6 +84,7 @@ class PushNotifier extends Controller
     	$antar_code = $slot->slot_id;
     	$sms_message = "Sisa waktu Anda telah habis untuk pengambilan barang antaran TIPS dengan kode pendaftaran penerbangan $antar_code pada TIPS Counter, kesediaan Anda menjadi TIPSTER otomatis telah dibatalkan karena tidak melakukan pengambilan.";
     	$this->delivery_sms_sender($sms_message, $user);
+        $this->bsc->sendMailTipsterNoPickupCancelled($user->email, $user->first_name . " " . $user->last_name, $antar_code);
     }
 
     public function _rejection_or_return_to_sender_dg($user, $shipment){
@@ -83,6 +95,7 @@ class PushNotifier extends Controller
         $ncc = "+62 823 1777 6008";
         $sms_message = "Paket kiriman anda tidak dapat kami proses lanjut karena teridentifikasi sebagai kategori DG (Dangerous Goods). Untuk info lebih lanjut mohon cek email inbox Anda atau hubungi tim kami di nomor $ncc";
         $this->shipment_sms_sender($sms_message, $user);
+        $this->bsc->sendMailShipperRejection($user->email, $user->first_name . " " . $user->last_name, $kirim_code, $ncc);
     }
 
     public function _no_shipment_for_tipster($user, $slot){
